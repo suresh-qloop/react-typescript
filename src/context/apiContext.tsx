3803709528b01852ffd0b9d906d4c31735756093
SmartDev <queueloopsolutions@gmail.com>
2022-10-01 10:28:02 +0530

    reload feature added

--- src/context/apiContext.tsx
+++ src/context/apiContext.tsx
@@ -2,31 +2,42 @@ import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
 interface AppContextInterface {
-  data: string[];
-  loading: boolean;
-}
+    data: string[];
+    loading: boolean;
+    getData: () => void;
+  }
 
 export const UserContext = createContext({} as AppContextInterface);
 
 export const UserContextProvider = (props: any) => {
-  const [data, setData] = useState<string[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+    const [data, setData] = useState<string[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        console.log(res);
-        setData(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+    useEffect(() => {
+      getData()
+    }, []);
+
+
+    const getData = ()=>{
+      console.log('on click');
+      
+      setData([])
+      setLoading(true)
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => {
+          setData(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+    }
+
 
   return (
-    <UserContext.Provider value={{ data, loading }}>
+    <UserContext.Provider value={{ data , loading , getData }}>
       {props.children}
     </UserContext.Provider>
   );
